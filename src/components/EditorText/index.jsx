7c6f079777d1eabe67aa81b4433d32a76eaafe9b
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2024-07-24 07:38:02 -0700

    add text Editor in modal of button create

--- src/components/EditorText/index.jsx
+++ src/components/EditorText/index.jsx
@@ -1,8 +1,42 @@
-import React, { useRef } from 'react';
-import { SafeAreaView, Button } from 'react-native';
+import React, { useRef } from "react";
+import { SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
+import QuillEditor, { QuillToolbar } from "react-native-cn-quill";
+import tw from "twrnc";
 
 const EditorText = () => {
-
+  const _editor = useRef();
+  return (
+    <SafeAreaView style={styles.root}>
+      <StatusBar style="auto" />
+      <View style={styles.toolbarContainer}>
+        <QuillToolbar editor={_editor} options="full" theme="light" />
+      </View>
+      <QuillEditor
+        style={styles.editor}
+        ref={_editor}
+        initialHtml="<h1>Quill Editor for react-native</h1>"
+      />
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: "#eaeaea",
+  },
+  toolbarContainer: {
+    borderBottomWidth: 1,
+    borderColor: "gray",
+    backgroundColor: "#fff",
+  },
+  editor: {
+    flex: 1,
+    padding: 10,
+    borderColor: "gray",
+    borderWidth: 1,
+    backgroundColor: "white",
+  },
+});
 
 export default EditorText;
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -2,7 +2,7 @@ import { View, Text, Modal, TouchableOpacity, Button } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import { AddIcon } from "../Icons";
-import EditorText from '../EditorText'
+import EditorText from '../EditorText';
 
 const Modals = () => {
   const [modalVisible, setModalVisible] = React.useState(false);
@@ -10,7 +10,7 @@ const Modals = () => {
   return (
     <View>
       <TouchableOpacity
-        style={tw` rounded bg-zinc-100 dark:bg-black`}
+        style={tw`rounded bg-zinc-100 dark:bg-black p-3`}
         onPress={() => {
           setModalVisible(true);
           console.log("Agregar nota");
@@ -30,15 +30,15 @@ const Modals = () => {
         <View
           style={tw`flex-1 justify-center items-center bg-gray-800 bg-opacity-50`}
         >
-          <View style={tw`w- bg-white p-5 rounded-lg`}>
-            <Text style={tw`text-lg font-bold`}>Modal Title</Text>
-            <EditorText/>
+          <View style={tw`w-full h-3/4 bg-white p-5 rounded-lg`}>
+            <Text style={tw`text-lg font-bold mb-4`}>Modal Title</Text>
+            <EditorText />
             <Button
               title="Close Modal"
               onPress={() => {
                 setModalVisible(!modalVisible);
               }}
-            ></Button>
+            />
           </View>
         </View>
       </Modal>
